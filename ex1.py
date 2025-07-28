37c495e8f792ad230e9a4330f42bfc9d13407b72
MasumPatel305 <12310080@cqumail.com>
2025-07-28 11:26:04 +1000

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,96 @@
+# print('hello')
+
+
+# def check_sign(num):
+#     if num > 0:
+#         return 'positive'
+#     elif num == 0:
+#         return 'zero'
+#     else:
+#         return 'negative'
+
+# print(check_sign(0))
+
+
+# def check_temp(temp):
+#     if(temp<0):
+#         return 'Freezing'
+#     elif(temp>=0 and temp<= 15):
+#         return 'Cold'
+#     elif(temp>=16 and temp<= 25):
+#         return 'Warm'
+#     else:
+#         return 'Hot'
+
+# print(check_temp(44))
+
+# def converter(stra):
+#      a = float(stra)
+#      return a**2
+
+# print(converter('3.7'))
+# def converter(stra):
+#      a = int(stra)
+#      return a
+
+# print(converter(False))
+
+# score = 89.5
+# print("Float to int:", int(score))  
+
+# def converter(input):
+#      a = float(input)
+#      return str(a)
+
+# print(converter(False))
+
+# squares = [x**2 for x in range(5) if x%2 == 0]
+# print("Even squares:", squares)
+
+# nums = [1, -2, 3, 4, -5]
+
+# a = [x for x in nums if x%2 != 0 and x<0]
+# print(a)
+
+# prices = [20, 55, 80, 100]
+
+# discount = [x*0.8 for x in prices if x>=50 ]
+
+# print(discount)
+
+# fruits = ['apple', 'banana', 'cherry']
+
+# del fruits[2]
+
+# print("Updated fruits:", fruits)
+# print("Last fruit:", fruits[-2])
+
+# fruits.append('date')
+# print("Updated fruits:", fruits)
+
+# marks = [60, 80, 45, 90, 77]
+# print("Highest mark:", max(marks))
+# print("Lowest mark:", min(marks))
+# print("Top three ", sorted(marks, reverse=True)[:3])
+
+# mylist = []
+# for i in range(10,100):
+#     mylist.append(i)
+
+# print(mylist)
+
+# data = ['a', 'b', 'c', 'd']
+
+# reversed_data = data[::-1]
+# print(list(reversed(data)))
+# print("Reversed list:", reversed_data)
+# print("Reversed list:", reversed_data[1])
+
+# text = "Hello world"
+# text = text.upper()
+# print("is title case: ", text.istitle())
+
+# uinput = str("Qhello")
+# print(uinput.isupper())
+# print(uinput.startswith('Q'))
+# print(uinput.isalpha())
